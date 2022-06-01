dbc6daa2c3a1b6139f9c27db734dfc18ef0c424b
Shawn Argent <kshawn001@gmail.com>
2022-05-31 21:29:40 -0700

    setup

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <h2 className="about-header">About</h2>
+      <p>Current UCSD|Extension, Full Stack Flex Coding Bootcamp student.</p>
+    </div>
+  );
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <contact className="homepage-contact">
+      <h2>Contact</h2>
+      <br />
+      <span>Email: </span>kshawn001@gmail.com
+      <br />
+      <a href="">GitHub</a>
+      <br />
+      <a href="">LinkedIn</a>
+      <br />
+      <a href="">Resume</a>
+    </contact>
+  );
+}
