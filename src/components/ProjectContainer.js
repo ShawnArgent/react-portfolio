678d557148d4e246be95e3e53dec26fbeca997a2
Shawn Argent <kshawn001@gmail.com>
2022-06-06 10:03:24 -0700

    images working

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,17 +1,27 @@
-import uniqid from 'uniqid';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LaunchIcon from '@material-ui/icons/Launch';
-import './ProjectContainer.css';
+import uniqid from 'uniqid'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LaunchIcon from '@material-ui/icons/Launch'
+import './ProjectContainer.css'
 
 const ProjectContainer = ({ project }) => (
-  <div className="project">
+  <div className='project'>
     <h3>{project.name}</h3>
 
-    <p className="project__description">{project.description}</p>
+    <div className='img-container'>
+      <img
+        src={project.image}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '125%',
+        }}
+        alt={project.name}
+      />
+    </div>
+
     {project.stack && (
-      <ul className="project__stack">
+      <ul className='project__stack'>
         {project.stack.map((item) => (
-          <li key={uniqid()} className="project__stack-item">
+          <li key={uniqid()} className='project__stack-item'>
             {item}
           </li>
         ))}
@@ -19,17 +29,25 @@ const ProjectContainer = ({ project }) => (
     )}
 
     {project.sourceCode && (
-      <a href={project.sourceCode} aria-label="source code" className="link link--icon">
+      <a
+        href={project.sourceCode}
+        aria-label='source code'
+        className='link link--icon'
+      >
         <GitHubIcon />
       </a>
     )}
 
     {project.livePreview && (
-      <a href={project.livePreview} aria-label="live preview" className="link link--icon">
+      <a
+        href={project.livePreview}
+        aria-label='live preview'
+        className='link link--icon'
+      >
         <LaunchIcon />
       </a>
     )}
   </div>
-);
+)
 
-export default ProjectContainer;
+export default ProjectContainer
