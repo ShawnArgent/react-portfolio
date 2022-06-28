cfa7f4779f8798e890a775842483c9fd3f018d57
Shawn Argent <kshawn001@gmail.com>
2022-06-27 17:47:01 -0700

    deploy

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,8 @@ import image from '../../assets/MyAltLogo.jpg'
 const Header = (props) => {
     return (
       <header className='flex-row space-between px-1'>
-        <a href='https://shawnargent.github.io/react-portfolio/'>
-      <img src={image} width={150} height={150} alt="logo"/>
+        <a className='mylogo' href='https://shawnargent.github.io/react-portfolio/'>
+      <img src={image} width={200} height={200} alt="logo"/>
           </a>
       {props.children}
           
